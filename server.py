643fb6ea1c39d078d7d4ceab2bc3e9a4e3512021
kobalts <agrandans@gmail.com>
2026-02-09 06:00:29 -0500

    Task6 almost done

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, url_for,
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector as ed
 
 app=Flask(__name__)
 
-@app.route('/emotionDetector',  methods=['GET,''POST'])
-    return
 
+@app.route("/",  methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/emotionDetector",  methods=["GET"])
+def check_emotions():
+    text=request.args.get("textToAnalyze", "", type=str)
+    #text = request.form.get("textToAnalyze", "")
+    
+    if text is None:
+        result="no text provided to analyze"
+        return result
+    else:
+        result=ed(text)
+        return result
 
 
 if __name__=="__main__":
