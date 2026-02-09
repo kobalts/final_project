9eb8ffb85cac76ec61857e0b1d8cb441620d9b58
kobalts <agrandans@gmail.com>
2026-02-09 07:52:30 -0500

    Task 6  done

--- server.py
+++ server.py
@@ -12,14 +12,22 @@ def index():
 @app.route("/emotionDetector",  methods=["GET"])
 def check_emotions():
     text=request.args.get("textToAnalyze", "", type=str)
-    #text = request.form.get("textToAnalyze", "")
-    
+        
     if text is None:
         result="no text provided to analyze"
         return result
     else:
-        result=ed(text)
-        return result
+        full_response=ed(text)
+        result = ", ".join(
+            f"{key}: {val}"
+            for key, val in full_response.items()
+            if key != "dominant_emotion"
+        )
+
+        return (
+            f"For the given statement, the system response is {result}. "
+            f"The dominant emotion is {full_response['dominant_emotion']}."
+        )
 
 
 if __name__=="__main__":
